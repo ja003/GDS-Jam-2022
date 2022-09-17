6912f0a5df46698d9915e27131742a5f852f5ec1
Adam Jurík <ja004@seznam.cz>
2022-09-17 20:50:50 +0200

    Main menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Button btnPlay;
+    [SerializeField] Button btnControls;
+
+    private void Awake()
+    {
+        btnPlay.onClick.AddListener(Play);
+        btnControls.onClick.AddListener(Play);
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadScene("S_Game");
+    }
+
+    private void Controls()
+    {
+        SceneManager.LoadScene("S_Controls");
+    }
+}
