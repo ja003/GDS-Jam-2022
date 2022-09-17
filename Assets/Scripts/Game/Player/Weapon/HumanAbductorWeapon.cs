f354793b9780e713d8e90db7695cd67d10c893f8
Adam Jurík <ja004@seznam.cz>
2022-09-17 15:27:36 +0200

    simple abduction

--- Assets/Scripts/Game/Player/Weapon/HumanAbductorWeapon.cs
+++ Assets/Scripts/Game/Player/Weapon/HumanAbductorWeapon.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class HumanAbductorWeapon : WeaponBase
 {
 	[SerializeField] float range = 2;
+	[SerializeField] LayerMask ScientistLayer;
 	protected override void Use(Vector3 pDirection)
 	{
 		Debug.DrawLine(transform.position, transform.position + pDirection.normalized * range, Color.red, 1);
 		Debug.Log("ABDUCT");
+
+		RaycastHit hit;
+
+		if(Physics.Raycast(new Ray(transform.position, pDirection.normalized * range), out hit, range, (LayerMask)ScientistLayer))
+		{
+			Destroy(hit.transform.gameObject);
+		}
 	}
 }
