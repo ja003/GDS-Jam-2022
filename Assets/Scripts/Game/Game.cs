d44af55adaa8ab6dfba79b617bd5d94729a4a571
Adam Jurík <ja004@seznam.cz>
2022-09-17 21:17:05 +0200

    end game

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Game : CSingleton<Game>
 {
@@ -16,10 +18,30 @@ public class Game : CSingleton<Game>
 
 	[SerializeField] GameObject EndGameScreen;
 
-	internal void EndGame()
+    [SerializeField] Button btnPlay;
+    [SerializeField] Button btnMenu;
+
+    private void Awake()
+    {
+        EndGameScreen.SetActive(false);
+        btnPlay.onClick.AddListener(PlayAgain);
+        btnMenu.onClick.AddListener(Menu);
+    }
+
+    internal void EndGame()
 	{
 		HUD.OnEndGame();
 		Debug.Log("End game");
 		EndGameScreen.SetActive(true);
+    }
+
+    private void PlayAgain()
+    {
+        SceneManager.LoadScene("S_Game");
+    }
+
+    private void Menu()
+    {
+        SceneManager.LoadScene("S_Menu");
     }
 }
