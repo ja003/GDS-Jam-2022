4772437958179930ffc5db61d3ff5756638bb057
Adam Jurík <ja004@seznam.cz>
2022-09-17 11:46:03 +0200

    emp pulse electricity

--- Assets/Scripts/Game/Player/Weapon/EMPPulseWeapon.cs
+++ Assets/Scripts/Game/Player/Weapon/EMPPulseWeapon.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class EMPPulseWeapon : WeaponBase
 {
+	[SerializeField] ParticleSystem particles;
 	protected override void Use(Vector3 pDirection)
 	{
 		Debug.Log("USE");
+		particles.enableEmission = true;
+
+		DoInTime(() =>
+		{
+			particles.enableEmission = false;
+		}, 1);
 	}
 
 }
