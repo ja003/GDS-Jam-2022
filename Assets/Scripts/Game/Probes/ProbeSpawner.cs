e12b13c57c22af9e32556c162177216bd55ab2ed
charliepetr <charlie.k.petr@gmail.com>
2022-09-17 13:08:48 +0200

    basic probe movement

---
+++ Assets/Scripts/Game/Probes/ProbeSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbeSpawner : MonoBehaviour
+{
+	[SerializeField] private float cooldown;
+	[SerializeField] private float radius;
+
+	[SerializeField] private BasicProbe probePrefab;
+
+	float cooldownRemainig;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        cooldownRemainig = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownRemainig -= Time.deltaTime;
+
+		if (cooldownRemainig <= 0f)
+		{
+			float randomAngle = Random.Range(0f, 2 * Mathf.PI);
+			Vector3 direction = new Vector3(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle));
+			Vector3 spawnPoint = direction * radius;
+
+			Object.Instantiate(probePrefab, spawnPoint, probePrefab.GetComponent<Transform>().rotation);
+
+			cooldownRemainig = cooldown;
+		}
+    }
+}
