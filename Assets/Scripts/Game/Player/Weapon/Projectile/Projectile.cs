61bb20f5f3eba2bd010ebdf814fcfb6637c6d5e7
Adam Jurík <ja004@seznam.cz>
2022-09-18 08:21:20 +0200

    rocket rotation

--- Assets/Scripts/Game/Player/Weapon/Projectile/Projectile.cs
+++ Assets/Scripts/Game/Player/Weapon/Projectile/Projectile.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Projectile : GameBehaviour
 {
+	[SerializeField] Vector3 angleOffset = new Vector3(90,0,0);
 	int damage;
+
 	public void Shoot(Vector3 pForce, int pDamage)
 	{
 		rb.AddForce(pForce);
 		damage = pDamage;
+
+		transform.Rotate(Quaternion.LookRotation(pForce.normalized).eulerAngles + angleOffset);
 	}
 
 	private void OnCollisionEnter(Collision collision)
