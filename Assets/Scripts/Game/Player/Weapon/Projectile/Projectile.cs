7cc33ea5aff12c2efa86085ab6a88a04c7b65236
Adam Jurík <ja004@seznam.cz>
2022-09-18 16:08:08 +0200

    projectile trigger

--- Assets/Scripts/Game/Player/Weapon/Projectile/Projectile.cs
+++ Assets/Scripts/Game/Player/Weapon/Projectile/Projectile.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 public class Projectile : GameBehaviour
 {
 	[SerializeField] Vector3 angleOffset = new Vector3(90,0,0);
+	[SerializeField] float speed = 1;
 	int damage;
 
-	public void Shoot(Vector3 pForce, int pDamage)
+	Vector3 Direction;
+
+    public void Shoot(Vector3 pForce, int pDamage)
 	{
-		rb.AddForce(pForce);
+		Direction = pForce;
+
+        //rb.AddForce(pForce);
 		damage = pDamage;
 
 		transform.Rotate(Quaternion.LookRotation(pForce.normalized).eulerAngles + angleOffset);
 	}
 
-	private void OnCollisionEnter(Collision collision)
+	private void Update()
 	{
-		//Debug.Log("OnCollisionEnter " + collision.gameObject.name);
-		IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+		transform.position += Direction.normalized * speed;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        Debug.Log("OnTriggerEnter " + other.gameObject.name);
+        IDamagable damagable = other.gameObject.GetComponent<IDamagable>();
 
         if (damagable != null)
-			damagable?.OnHit(damage);
+            damagable?.OnHit(damage);
+
+
+
+        Destroy(gameObject);
+    }
+
+	//private void OnCollisionEnter(Collision collision)
+	//{
+	//	//Debug.Log("OnCollisionEnter " + collision.gameObject.name);
+	//	IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+
+ //       if (damagable != null)
+	//		damagable?.OnHit(damage);
 		
 
 
-		Destroy(gameObject);
-	}
+	//	Destroy(gameObject);
+	//}
 }
